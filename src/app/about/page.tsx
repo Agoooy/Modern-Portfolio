b1a54e45ebe0daeb04e31fb602dbbd64b3436b74
Yoga Ardika Pradana <55076240+Agoooy@users.noreply.github.com>
2024-07-20 04:08:05 +0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import About from "@/features/About";
 export const metadata = {
   title: "About | Yoga Ardika",
   description:
-    "I Am a Full-Stack Developer with extensive experience in developing web and mobile applications from concept to final implementation. Proficient in various front-end and back-end technologies, I have the ability to design, develop, and maintain efficient, responsive, and scalable applications.",
+    "I am a Full-Stack Developer with extensive experience in developing web and mobile applications from concept to final implementation. Proficient in various front-end and back-end technologies, I have the ability to design, develop, and maintain efficient, responsive, and scalable applications.",
 };
 
 const AboutPage = () => {

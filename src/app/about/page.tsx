0d998bd34bda57c701f4f21051ecb796c8afc315
Yoga Ardika Pradana <55076240+Agoooy@users.noreply.github.com>
2024-07-20 04:07:50 +0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import About from "@/features/About";
 export const metadata = {
   title: "About | Yoga Ardika",
   description:
-    "About Yoga Ardika | Hello! My real name is Yoga Ardika Pradana, an Informatics Engineering student interested and experienced in web development, UI design, and 3D modeling.",
+    "I Am a Full-Stack Developer with extensive experience in developing web and mobile applications from concept to final implementation. Proficient in various front-end and back-end technologies, I have the ability to design, develop, and maintain efficient, responsive, and scalable applications.",
 };
 
 const AboutPage = () => {
